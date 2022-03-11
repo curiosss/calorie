33fb2712f73b59f186d93f1902a1688cb0785cc3
curiosss <farhatbaymuradov4@gmail.com>
2022-03-11 19:14:42 +0500

    this

--- lib/helpers/image_helper.dart
+++ lib/helpers/image_helper.dart
@@ -1,11 +1,8 @@
-import 'dart:io';
-
-class ImageHelper{
-  static void init()async{
+class ImageHelper {
+  static void init() async {
     //  Directory documentsDirectory = await getApplicationDocumentsDirectory();
     // String path = join(documentsDirectory.path, "database.db");
   }
 }
 
-getApplicationDocumentsDirectory() {
-}
+getApplicationDocumentsDirectory() {}
